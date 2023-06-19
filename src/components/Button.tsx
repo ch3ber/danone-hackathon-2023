a0ede40f1073e395061f93d91d67036a4d606835
Eber Alejo <eber4202@hotmail.com>
2023-06-19 06:58:27 -0600

    STYLES: change button and home styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,28 +6,28 @@ type ButtonProps = {
   size?: 'small' | 'middle' | 'large'
 }
 
-export default function Button ({ children, type = 'fill', size = 'middle' }: ButtonProps) {
+export default function Button ({ children, type = 'fill', size = 'small' }: ButtonProps) {
   const buttonSizes = {
-    small: 'w-24 h-8',
-    middle: 'w-32 h-10',
-    large: 'w-40 h-12'
+    small: 'py-2 px-4',
+    middle: 'py-4 px-8',
+    large: 'py-6 px-12'
   }
 
-  const baseStyles = `py-13 px-23 text-button rounded-sm ${buttonSizes[size]}`
+  const baseStyles = `w-fit mx-auto text-button rounded-sm ${buttonSizes[size]}`
 
   if (type === 'fill') {
     return (
-      <button className={`${baseStyles} text-white bg-[#324BAA]`}>
+      <div className={`${baseStyles} text-white bg-[#324BAA]`}>
         {children}
-      </button>
+      </div>
     )
   }
 
   if (type === 'outline') {
     return (
-      <button className={`${baseStyles} outline outline-1 outline-[#324BAA] text-[#324BAA]`}>
+      <div className={`${baseStyles} outline outline-1 outline-[#324BAA] text-[#324BAA]`}>
         {children}
-      </button>
+      </div>
     )
   }
 }
