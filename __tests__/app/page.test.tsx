b5aec59e74fe453ae022cb0e2d9c7bea6c8642cb
Eber Alejo <eber4202@hotmail.com>
2023-06-19 00:31:04 -0600

    TEST: update home page unit test

--- __tests__/app/page.test.tsx
+++ __tests__/app/page.test.tsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import fetchMock from 'jest-fetch-mock'
 import Home from '@/app/page'
-
-fetchMock.enableMocks()
 
 describe('Home', () => {
   it('renders a heading', async () => {
-    const mockData = [{ id: 1, name: 'Mock Data 1' }, { id: 2, name: 'Mock Data 2' }]
-    fetchMock.mockResponse(JSON.stringify(mockData))
-
     render(<Home />)
 
-    expect(await screen.findByText('Danone hackathon')).toBeInTheDocument()
+    expect(await screen.findByText('Keep track of your daily calorie limit!')).toBeInTheDocument()
   })
 })
