22eaed8e6f59ef837e2edb35ddca4480d4ce5464
Eber Alejo <eber4202@hotmail.com>
2023-06-19 07:13:58 -0600

    FIX: Bearer token

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -3,7 +3,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 const httpLink = new HttpLink({
   uri: `${process.env.NEXT_PUBLIC_SERVICE_URL}/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID}/environments/${process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT}`,
   headers: {
-    Authorization: 'Bearer KYCdTWuVRddXZ2519o1ohKSIbZJFmKDgUaM4iejywHw'
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN}`
   }
 })
 
